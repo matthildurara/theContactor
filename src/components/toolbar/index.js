c51712f6629061cff998338a1c1e0c1e6a32a4d9
Matthildur <matthildura19@ru.is>
2021-11-29 13:28:08 +0000

    ttolbar

--- src/components/toolbar/index.js
+++ src/components/toolbar/index.js
@@ -1,10 +1,32 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import {View,Text,TouchableOpacity} from 'react-native';
+import {View,Text,TouchableOpacity,TextInput} from 'react-native';
 import {AntDesign} from '@expo/vector-icons';
 import styles from './styles';
 
-const Toolbar = ({ onPress,icon,text
+const Toolbar = ({ value,onSearch,onAdd
 
 
-})
+}) => (
+
+    <View style={styles.toolbar}>
+      <TouchableOpacity style={styles.toolbarAction}>
+        <TextInput
+          value={search}
+          setValue={setSearch}
+          placeholder="Search"
+          search={search}
+          />
+          </TouchableOpacity>
+
+          <TouchableOpacity
+          style={styles.toolbarAction}
+          onPress={() => onAdd}>
+            <Text> Add new Contact </Text>
+          </TouchableOpacity>
+
+          </View>
+
+);
+}
+export default Toolbar;
--- src/views/Contacts/index.js
+++ src/views/Contacts/index.js
@@ -1,17 +1,26 @@
-import React from 'react';
+import React {useState} from 'react';
 import {View,Text,TouchableHighlight,Image} from 'react-native';
 //import logo from '../../resources/logo.png';
 import styles from './styles';
+import Toolbar from '../../components/Toolbar';
+import AddContactModal from '../../components/AddContactModal';
 
 
-const Main = () => (
 
 
+const Main = () => {
+
+const [isAddModalOpen,setIsAddModalOpen] = useState(false);
+
+return (
   <View style={styles.container}>
+    <Toolbar
+    onAdd={()=> setIsAddModalOpen(true)}
     <Text style={styles.paragraph}> The Contactor </Text>
 
   </View>
-);
+)
+};
 
 
 
