e91b0b6d4047590a6858aa796d29e5e7141a0e8f
berthamaria <54938563+berthamaria@users.noreply.github.com>
2021-12-03 09:32:24 +0000

    fixing toolbar

--- src/components/Search/index.js
+++ src/components/Search/index.js
@@ -1,18 +1,21 @@
 import React from 'react';
 import {SearchBar as NativeSearchBar} from 'react-native-elements';
+import { MaterialIcons } from '@expo/vector-icons';
 
 
 const SearchBar = ({value, onSearch}) => (
-
+<View>
   <NativeSearchBar
     round
     lightTheme
-    placeholder="Search..."
+    //placeholder="Search..."
     onChangeText={onSearch}
     value={value}
     showLoading={false}
     containerStyle={{ borderWidth: 0.5, borderRadius: 1 }}
   />
+  <MaterialIcons name="search" size={24} color="black" />
+  </View>
 
 
 );
